b68279259bd37de95ba921fade34230fa59d8948
Annie Huynh <150502695+ahuynh3a@users.noreply.github.com>
2024-03-04 19:36:08 +0000

    Generated comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :photos
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -6,5 +7,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root "photo#index"
+  root "photos#index"
 end
