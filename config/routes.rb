fe3bc35aa83961c78513ad02dabc110af99cff3b
Annie Huynh <150502695+ahuynh3a@users.noreply.github.com>
2024-03-07 17:54:02 +0000

    removed unnecessary comments

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,4 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
   
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
 end
